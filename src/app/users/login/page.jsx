32d432a2a221a9cec335c0439d2f0b67bd2a8d89
suryanarayanan035 <suryanarayanan035@gmail.com>
2024-12-06 23:06:39 +0530

    implement login page

---
+++ src/app/users/login/page.jsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+import LoginForm from './LoginForm';
+
+export default function Page() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <LoginForm />
+    </div>
+  );
+}
