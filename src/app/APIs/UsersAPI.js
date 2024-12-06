f4d88e1565c06a60c29b2db2ddf474e2f050c164
suryanarayanan035 <suryanarayanan035@gmail.com>
2024-12-06 22:22:02 +0530

    implemented UsersAPI class

---
+++ src/app/APIs/UsersAPI.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+export default class UsersAPI {
+  constructor() {
+    this.API_PATH = 'users/';
+    this.axiosInstance = axios.create({
+      baseURL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL + this.API_PATH,
+    });
+  }
+
+  static handleError(error) {
+    if (error.response) {
+      const { status, statusText, data } = error.response;
+      return {
+        statusCode: status,
+        message: statusText,
+        data,
+      };
+    }
+
+    if (error.request) {
+      return {
+        statusCode: 0,
+        message: 'NETWORK ERROR',
+      };
+    }
+
+    return {
+      message: 'REQUEST IS NOT SETUP CORRECTLY',
+    };
+  }
+
+  async loginUser(username, password) {
+    try {
+      const { data } = await this.axiosInstance.post('login/', {
+        username,
+        password,
+      });
+      return { data };
+    } catch (error) {
+      return { error: this.handleError(error) };
+    }
+  }
+}
