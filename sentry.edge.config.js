0f3e4fef3b3dd54fc927fd1f9d1e2c8812e572bd
suryanarayanan035 <suryanarayanan035@gmail.com>
2024-12-06 22:26:55 +0530

    fix eslint issues in sentry

--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -1,17 +1,21 @@
-// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// This file configures the initialization of Sentry for
+// edge features (middleware, edge routes, and so on).
 // The config you add here will be used whenever one of the edge features is loaded.
-// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// Note that this config is unrelated to the Vercel Edge Runtime and
+// is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 // https://ddecb2b6e24f04df018308c3ed104e2d@o4508366075396096.ingest.us.sentry.io/4508366075658240
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  // Define how likely traces are sampled. Adjust this value in production,
+  // or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  // Setting this option to true will print useful information to the console
+  // while you're setting up Sentry.
   debug: false,
 });
