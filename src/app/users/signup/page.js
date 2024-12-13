c8e99abb0f38d160bdf0c9c2fb05e6da4cd8d246
Divya <divya.s@rently.com>
2024-12-13 22:45:31 +0530

    PMS-16 #comment Signup page with basic UI

--- src/app/users/signup/page.js
+++ src/app/users/signup/page.js
@@ -1,3 +1,14 @@
+'use client';
+
+import Signup from "./SignupForm";
+
 export default function SignupPage() {
-  return <form />;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center space-y-7 bg-[#e9d5ff]">
+      <p className="font-semibold text-[#7e22ce]">
+        Begin your journey with Post Master!
+      </p>
+      <Signup />
+    </div>
+  );
 }
