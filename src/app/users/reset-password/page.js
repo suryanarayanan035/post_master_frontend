640be18a775232fcc7043a03bcec71f924d17785
Divya <divya.s@rently.com>
2024-12-14 15:59:21 +0530

    PMS-26 #comment basic reset password page UI

---
+++ src/app/users/reset-password/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ResetPasswordPage from './ResetPasswordPage';
+
+function ResetPassword() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center space-y-7 bg-[#e9d5ff]">
+      <ResetPasswordPage />
+    </div>
+  );
+}
+export default ResetPassword;
