d291ec988d9cf4063895daa680cb084db99ea8a3
Kishorekumar-rently <kishorekumar.s@rently.com>
2024-12-21 23:52:58 +0530

    PMS-24 #comment add redirect to linkedIn account

--- src/app/users/dashboard/page.js
+++ src/app/users/dashboard/page.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 function Dashboard() {
+  const redirectURLToLinkedIn = 'localhost:8000/external_app/authorize'
   return (
     <div className="flex min-h-screen items-center justify-center">
       Dashboard screen
+      <Link href={redirectURLToLinkedIn}>
+        <Button type='secondary'>
+          connect LinkedIn account
+        </Button>
+      </Link>
     </div>
   );
 }
